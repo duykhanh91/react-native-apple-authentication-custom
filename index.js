451719b1da5f85aa831019de0929534d64ce38a6
duy khanh <khanh.huynh@amagumolabs.com>
2019-12-01 12:48:56 +0700

    add new lib

---
+++ index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NativeModules, requireNativeComponent ,Platform } from 'react-native';
+
+const { AppleAuthentication } = NativeModules;
+
+export const RNSignInWithAppleButton = requireNativeComponent('RNCSignInWithAppleButton');
+
+export const SignInWithAppleButton = (buttonStyle, callBack) => {
+  if(Platform.OS === 'ios'){
+    return <RNSignInWithAppleButton style={buttonStyle} onPress={async () => {
+    
+        await AppleAuthentication.requestAsync({
+          scopes: [AppleAuthentication.Scope.FULL_NAME, AppleAuthentication.Scope.EMAIL],
+        }).then((response) => {
+          callBack(response) //Display response
+          }, (error) => {
+            callBack(error) //Display error
+           
+        });
+
+  }} />
+  }else{
+  return null
+
+  }
+   
+}
+
+export default AppleAuthentication;
